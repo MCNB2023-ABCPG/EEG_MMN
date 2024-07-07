2b0bbe9ebc45ab4446f4c7ea5fed4ec30e087db4
greta-gehr <154230769+greta-gehr@users.noreply.github.com>
2024-07-07 18:12:06 +0200

    including crop

--- time_frequency_analysis.m
+++ time_frequency_analysis.m
@@ -2,7 +2,7 @@
 function time_frequency_analysis(folder_path_root, spm_path)
 % initialization
 if ~exist('spm_path', 'var')
-    spm_path = '/Users/angelaseo/Documents/spm-main';
+    spm_path = '/Users/greta/Desktop/spm12';
 end
 
 % set root path
@@ -30,27 +30,47 @@ for i = 1:numel(folder_base_sub)
     % select STATS directory
     folder_path_stats = fullfile(S.folder_path_data, S.folder_base_sub, 'STATS');
 
-    % Wavelet estimation
-    job = [];
-    preprocessed_file = spm_select('FPList', folder_path_pre, '^aefdfMspm.*\.mat$');
-    job{1}.spm.meeg.tf.tf.D = {preprocessed_file};
-    job{1}.spm.meeg.tf.tf.channels{1}.all = 'all';
-    job{1}.spm.meeg.tf.tf.frequencies = [6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40];
-    job{1}.spm.meeg.tf.tf.timewin = [-Inf Inf];
-    job{1}.spm.meeg.tf.tf.method.morlet.ncycles = 5;
-    job{1}.spm.meeg.tf.tf.method.morlet.timeres = 0;
-    job{1}.spm.meeg.tf.tf.method.morlet.subsample = 5;
-    job{1}.spm.meeg.tf.tf.phase = 1;
-    job{1}.spm.meeg.tf.tf.prefix = '';
+    % % Wavelet estimation
+    % job = [];
+    % preprocessed_file = spm_select('FPList', folder_path_pre, '^aefdfMspm.*\.mat$');
+    % job{1}.spm.meeg.tf.tf.D = {preprocessed_file};
+    % job{1}.spm.meeg.tf.tf.channels{1}.all = 'all';
+    % job{1}.spm.meeg.tf.tf.frequencies = [6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40];
+    % job{1}.spm.meeg.tf.tf.timewin = [-Inf Inf];
+    % job{1}.spm.meeg.tf.tf.method.morlet.ncycles = 5;
+    % job{1}.spm.meeg.tf.tf.method.morlet.timeres = 0;
+    % job{1}.spm.meeg.tf.tf.method.morlet.subsample = 5;
+    % job{1}.spm.meeg.tf.tf.phase = 1;
+    % job{1}.spm.meeg.tf.tf.prefix = '';
+    % 
+    % spm_jobman('run', job);
+
+    % % move converted subject folder to STATS
+    % power_file = 'tf_aefdfMspmeeg_subject1.mat';
+    % movefile(fullfile(folder_path_pre, power_file), fullfile(folder_path_stats, power_file))
+    % 
+    % phase_file = 'tph_aefdfMspmeeg_subject1.mat';
+    % movefile(fullfile(folder_path_pre, phase_file), fullfile(folder_path_stats, phase_file))
+
+    % Crop
+    wavelet_estimated_power_file = spm_select('FPList', folder_path_stats, '^tf_aefdfMspm.*\.mat$');
+    job{1}.spm.meeg.preproc.crop.D = {wavelet_estimated_power_file};
+    job{1}.spm.meeg.preproc.crop.timewin = [-100 800];
+    job{1}.spm.meeg.preproc.crop.freqwin = [-Inf Inf];
+    job{1}.spm.meeg.preproc.crop.channels{1}.all = 'all';
+    job{1}.spm.meeg.preproc.crop.prefix = 'p';
+    spm_jobman('run', job);
+
+
+    wavelet_estimated_phase_file = spm_select('FPList', folder_path_stats, '^tph_aefdfMspm.*\.mat$')
+    job{2}.spm.meeg.preproc.crop.D = {wavelet_estimated_phase_file};
+    job{2}.spm.meeg.preproc.crop.timewin = [-100 800];
+    job{2}.spm.meeg.preproc.crop.freqwin = [-Inf Inf];
+    job{2}.spm.meeg.preproc.crop.channels{1}.all = 'all';
+    job{2}.spm.meeg.preproc.crop.prefix = 'p';
 
     spm_jobman('run', job);
 
-    move converted subject folder to STATS
-    power_file = 'tf_aefdfMspmeeg_subject1.mat';
-    movefile(fullfile(folder_path_pre, power_file), fullfile(folder_path_stats, power_file))
+    % Average
 
-    phase_file = 'tph_aefdfMspmeeg_subject1.mat';
-    movefile(fullfile(folder_path_pre, phase_file), fullfile(folder_path_stats, phase_file))
-
-    % Crop
 end
